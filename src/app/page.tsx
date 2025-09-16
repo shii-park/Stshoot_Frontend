83429ccc36e65ec9fd42e45cafc20bc2db32f199
Suiren91 <rento8191@gmail.com>
2025-09-16 16:24:40 +0900

    fix:ヘッダーの記述をlayout.tsxへ移行

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,17 +1,12 @@
 import Image from "next/image";
+import Link from "next/link"
 import MenuButton from "./menuButton"
+
 
 export default function Home() {
   return (
-    <div id="header">
-      <div className="flex box-border h-20 bg-white border-b-2">
-        <div className="mt-5 ml-5 text-lg md:ml-10 md:text-lg lg:ml-10 lg:text-5xl">
-          <h1>StShoot</h1>
-        </div>
-        <div className="ml-auto">
-          <MenuButton />
-        </div>
+      <div className="flex justify-center">
+        <Link href="./test"><p>こんにちは</p></Link>
       </div>
-    </div>
   );
 }
