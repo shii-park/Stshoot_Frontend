cf186587bd54e82de90d4134356661eda00cff00
Suiren91 <rento8191@gmail.com>
2025-09-17 02:41:46 +0900

    fix:トップページにログイン中のユーザが表示されるように #7

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -1,5 +1,5 @@
 import {initializeApp,getApps,getApp} from "firebase/app"
-import { getAuth } from "firebase/auth";
+import { getAuth} from "firebase/auth";
 
 const firebaseConfig={
     apiKey: process.env.NEXT_PUBLIC_API_KEY,
