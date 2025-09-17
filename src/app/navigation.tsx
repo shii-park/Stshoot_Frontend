dc8975723d31aca3dcf69a30b2cbb5a5fd642137
Suiren91 <rento8191@gmail.com>
2025-09-17 13:16:44 +0900

    add:ログアウト機能の実装 #7

--- src/app/navigation.tsx
+++ src/app/navigation.tsx
@@ -1,9 +1,22 @@
 import Link from "next/link"
+import {signOut} from "firebase/auth"
+import React from "react";
+import {auth} from "@/lib/firebase"
+
 type Props = {
   open: boolean;
 };
 
 export default function Navigation({ open}: Props) {
+const handleSignOut=async(e:React.FormEvent)=>{
+  try{
+    await signOut(auth);
+    alert("ログアウトしました");
+  }catch(err){
+    alert(err);
+  }
+}
+
   return (
     <nav
     className={`navigation${open ? "" : " hidden"}`}
@@ -12,6 +25,7 @@ export default function Navigation({ open}: Props) {
         <Link href="./login"><li>ログイン</li></Link>
         <li>works</li>
         <li>contact</li>
+        <button onClick={handleSignOut}><li>ログアウト</li></button>
       </ul>
     </nav>
   );
